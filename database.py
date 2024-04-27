c17be00c60c72a1a0e0ad39a9e928a3b45d2b206
AravindR97 <132665408+AravindR97@users.noreply.github.com>
2024-04-27 15:42:09 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,26 @@
-db = []
+import sqlite3 as sq
+
+# Establishing a connection with the database:
+conn = sq.connect("StudentJournalDB.db")
+        # conn :- connection object
+# context manager:
+with conn:
+    conn.execute("CREATE TABLE IF NOT EXISTS entries (date TEXT, content TEXT);")
+
+def close_connection():
+    conn.close()
 
 def add_entry(date, content):
-    """Function to add entries to the database
+    """Function to add entries to the SQLite database
     """
-    db.append({"date":date, "content":content})
+    with conn:
+        conn.execute("INSERT INTO entries VALUES(?, ?);", (date, content))
+        # ip variables provided in tuples as second argument.
+        # Do not use fstrings (Injection Attack!).
+        # Use tuples to give input even if there is only one variable
+            # Eg: (date,) -> single element tuple
 
 def get_entries():
-    return db
+    """Function to retrieve entries from the SQLite database
+    """
+    return conn.execute("SELECT * FROM entries;")
